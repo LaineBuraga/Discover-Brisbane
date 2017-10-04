302c2d2d1488bba7711caea3a23e961302628187
Jasper Stewart-Lane <jasperstewartlane@gmail.com>
2017-10-04 15:27:07 +1000

    no message

--- database dump/Dump20170915-1.sql
+++ database dump/Dump20170915-1.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ifb299` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ifb299`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: ifb299
@@ -85,4 +87,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-04 15:22:02
+-- Dump completed on 2017-10-04 15:26:52
