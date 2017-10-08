50d313a9d8a97f75823e354b49d98092dfc68765
Eelyn Lim <eelyn_lim@hotmail.com>
2017-10-09 03:19:57 +1000

    category list of locations - completed personalised pages - incomplete - still in progress

--- smartCity/discoverBris/views.py
+++ smartCity/discoverBris/views.py
@@ -1,55 +1,76 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render
-from .models import Locations, Client
+from .models import Location, Client
 from django.http import Http404
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'homepage/homepage.html', {})
+    all_locations = Location.objects.all()
+    context = {'all_locations': all_locations}
+    return render(request, 'homepage/homepage.html', context)
 
 def locations(request):
-    all_locations = Locations.objects.all()
+    all_locations = Location.objects.all()
     context = {'all_locations': all_locations}
     return render(request, 'locations/locations.html', context)
 
 def location(request, location_id):
     try:
-        location = Locations.objects.get(pk=location_id)
+        location = Location.objects.get(pk=location_id)
     except Locations.DoesNotExist:
         raise Http404("Location does not exist")
     return render(request, 'location/location.html', {'location': location})
 
-#admin --- test!
-def successView(request):
-    all_locations = Locations.objects.all()
-    return render(request, 'admin/adminPage.html', {'all_locations': all_locations})
 #result list --- test!
 def collegeList(request):
-    all_locations = Locations.objects.all()
-    return render(request, 'collegeList/collegeList.html', {'all_locations': all_locations})
+    all_locations = Location.objects.all()
+    return render(request, 'list/collegeList.html', {'all_locations': all_locations})
+
+def hotelList(request):
+     all_locations = Location.objects.all()
+     return render(request, 'list/hotelList.html', {'all_locations': all_locations})
+
+def industryList(request):
+  all_locations = Location.objects.all()
+  return render(request, 'list/industryList.html', {'all_locations': all_locations})
+
+def libraryList(request):
+   all_locations = Location.objects.all()
+   return render(request, 'list/libraryList.html', {'all_locations': all_locations})
+
+def mallList(request):
+    all_locations = Location.objects.all()
+    return render(request, 'list/mallList.html', {'all_locations': all_locations})
+
+def museumList(request):
+    all_locations = Location.objects.all()
+    return render(request, 'list/museumList.html', {'all_locations': all_locations})
 
 def parkList(request):
-    all_locations = Locations.objects.all()
-    return render(request, 'parkList/parkList.html', {'all_locations': all_locations})
+    all_locations = Location.objects.all()
+    return render(request, 'list/parkList.html', {'all_locations': all_locations})
 
+def restaurantList(request):
+    all_locations = Location.objects.all()
+    return render(request, 'list/restauranList.html', {'all_locations': all_locations})
 
-#old - test #1
-#def detail(request, client_id)
-#   all_clients = Client.objects.all()
-#    html = ''
-#    for client in all_clients:
-#        url = '/discoverBris/' + str(client_id) + '/'
-#        html += '<a href="' + url + '">' + client.client_type + '</a><br>'
-#    return HttpResponse(html)
-    #return HttpResponse(<h2>Details of customer ID" + str(client_id) + "</h2>")
+def zooList(request):
+    all_locations = Location.objects.all()
+    return render(request, 'list/zooList.html', {'all_locations': all_locations})
 
-#static pages
-#def result(request):
-#    return render(request, 'result/locationList.html', {})
+#account
+def business(request):
+    return render(request, 'account/business.html', {})
 
-#@csrf_protect
+def student(request):
+    return render(request, 'account/student.html', {})
+
+def tourist(request):
+    return render(request, 'account/tourist.html', {})
+#admin --- test!
 #def successView(request):
-#    return render(request, 'admin/adminPage.html', {})
+#    all_locations = Location.objects.all()
+#    return render(request, 'admin/adminPage.html', {'all_locations': all_locations})
