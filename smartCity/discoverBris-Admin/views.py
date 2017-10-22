3e4758980f0b9405cebcab605b648ee66e7b7bfb
Eelyn Lim <eelyn_lim@hotmail.com>
2017-10-22 13:40:26 +1000

    removed all old and unused items/templates

--- smartCity/discoverBris-Admin/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render
-
-
-# Create your views here.
-
-def index(request):
-    return render(request, 'admin/admin-login.html', {})
