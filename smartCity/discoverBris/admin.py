28ac9a0be0dc4da0e418f2a8dda5400b0d5158d8
Eelyn Lim <eelyn_lim@hotmail.com>
2017-10-23 15:12:14 +1000

    breadcrumbs link and images fixed

--- smartCity/discoverBris/admin.py
+++ smartCity/discoverBris/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AuthUser, Location, Feedback
+from .models import AuthUser, Client, Location, Feedback
 # Register your models here.
 
 admin.site.register(AuthUser)
+admin.site.register(Client)
 admin.site.register(Location)
 admin.site.register(Feedback)
