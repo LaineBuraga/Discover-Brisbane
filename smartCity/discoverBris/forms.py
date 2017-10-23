217d50708db2d0e4b643a5d72aa341dff956f3c9
Jasper Stewart-Lane <jasperstewartlane@gmail.com>
2017-10-23 15:15:28 +1000

    added confirm password field with working logic

--- smartCity/discoverBris/forms.py
+++ smartCity/discoverBris/forms.py
@@ -3,12 +3,23 @@ from django import forms
 
 class ClientForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password=forms.CharField(widget=forms.PasswordInput)
     Choices = (('Business', 'Business'),('Student', 'Student'),('Tourist', 'Tourist'),)
     client_type = forms.ChoiceField(choices=Choices)
 
     class Meta:
         model = Client
-        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'phone_number', 'client_type']
+        fields = ['username', 'email', 'first_name', 'last_name', 'phone_number', 'client_type', 'password']
+    
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
 
 class ClientLogin(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
