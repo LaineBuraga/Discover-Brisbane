a976e6aa5194411c922e3e78ccb5e3669167c469
Benedict Yeang <benedict.yeang@connect.qut.edu.au>
2017-10-24 21:33:24 +1000

    Sprint 2 models, SQL insertions and manual testing

---
+++ Sprint 2/database dump/SQL_Insertions.sql
@@ -0,0 +1,27 @@
+use ifb299;
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Novotel', '200 Creek St, Brisbane City QLD 4000', '07 3309 3309', 'novotel@accorshotel.com', 'Hotel', 'Novotel.jpg');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Mercure Brisbane', '85-87 N Quay, Brisbane City QLD 4000', '07 3237 2300', 'Mercure@accorshotel.com', 'Hotel', 'Mercure.jpg');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Hilton Brisbane', '190 Elizabeth St, Brisbane City QLD 4000', '07 3234 2000', 'brisbane@hilton.com', 'Hotel', 'Hilton.jpg');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Next Hotel Brisbane', '72 Queen St, Brisbane City QLD 4000', '07 3222 3222', 'brisbane@NextHotel.com', 'Hotel', 'next.jpg');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Taros Ramen', '480 Queen St, Brisbane City QLD 4000', '07 3839 4840', 'help@taros.com.au', 'Restaurant', 'Taros.jpg');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Madtongsan II', '1/85 Elizabeth St, Brisbane City QLD 4000', '07 3003 1881', 'tongsan@gmail.com', 'Restaurant', 'tongsan.jpg');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Fat Noodle', 'Treasury Casino, George St & Queen St, Brisbane City QLD 4000', '07 3306 8888', 'fatnoodle@treasury.com.au');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('322 Moggill Rd, Indooroopilly QLD 4068', '07 3378 4022', 'support@indroshopping.com.au', 'Mall', 'indro.png');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Westfield Chermside', 'Gympie Rd & Hamilton Rd, Chermside QLD 4032', '07 3117 5300', 'chermhelp@westfield.com.au', 'mall', 'WestfieldChermside.jpg');
+insert into location (name, address, phone_number, category, photo_name)
+values ('Woolcock Park', '92 Waterworks Rd, Red Hill QLD 4059', '07 3403 8888', 'Park', 'woolcock.png');
+insert into location (name, address, phone_number, category, photo_name)
+values ('Bushland Park', 'Bardon QLD 4065', '07 3403 8888', 'Park', 'bushland.png');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Lone Pine Koala Sanctuary', '708 Jesmond Rd, Fig Tree Pocket QLD 4069', '07 3378 1366', 'help@koala.net', 'Zoo', 'LonePine.jpg');
+insert into location (name, address, phone_number, email, category, photo_name)
+values ('Biambi Yumber Park', '30 Kenny St, Fig Tree Pocket QLD 4069', '07 3403 8888', 'help@brisbanekids.gov.au', 'Park', 'alonga,jpg');
